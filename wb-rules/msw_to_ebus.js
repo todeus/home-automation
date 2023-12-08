dacda0829baec74b8a877ac37fee0813f9a68af4
root <root@wbdevenv>
2023-12-08 16:11:16 +1000

    update climate page & boiler rule

--- wb-rules/msw_to_ebus.js
+++ wb-rules/msw_to_ebus.js
@@ -1,9 +1,7 @@
 
 var thermal_curve7 = [];
-var outide_temp;
 var setpoint;
 var delta;
-var set_temp;
 
 defineVirtualDevice("heat_floor", {
   title: "Heat Floor",
@@ -23,12 +21,9 @@ defineVirtualDevice("heat_floor", {
   }
 });
 
-
-set_temp = 0;
 k = 0;
 delta = 0;
 setpoint = 0;
-outside_temp = 0;
 
 thermal_curve7[10] = 29;
 thermal_curve7[8.5] = 30;
@@ -46,50 +41,52 @@ thermal_curve7[-20] = 41;
 thermal_curve7[-30] = 42;
 thermal_curve7[-40] = 43;
 
+function setTemperature(set, outside){
+  if((outside > -40) && (outside < -30)) {
+    setpoint = thermal_curve7[-40];
+  } else if ((outside > -30) && (outside < -20)) {
+    setpoint = thermal_curve7[-30];
+  } else if ((outside > -20) && (outside < -10.5)) {
+    setpoint = thermal_curve7[-20];
+  } else if ((outside > -10.5) && (outside < -6.5)) {
+    setpoint = thermal_curve7[-10.5];
+  } else if ((outside > -6.5) && (outside < -5)) {
+    setpoint = thermal_curve7[-6.5];
+  } else if ((outside > -5) && (outside < -3)) {
+    setpoint = thermal_curve7[-5];
+  } else if ((outside > -3) && (outside < 0)) {
+    setpoint = thermal_curve7[-3];
+  } else if ((outside > 0) && (outside < 1)) {
+    setpoint = thermal_curve7[0];
+  } else if ((outside > 1) && (outside < 2.5)) {
+    setpoint = thermal_curve7[1];
+  } else if ((outside > 2.5) && (outside < 5)) {
+    setpoint = thermal_curve7[2.5];
+  } else if ((outside > 5) && (outside < 7)) {
+    setpoint = thermal_curve7[5];
+  } else if ((outside > 7) && (outside < 8.5)) {
+    setpoint = thermal_curve7[7];
+  } else if ((outside > 8.5) && (outside < 10)) {
+    setpoint = thermal_curve7[8.5];
+  } else if (outside > 10) {
+    setpoint = thermal_curve7[10];
+  }
+  
+  k = dev["heat_floor"]["k"]/10;
+  delta = set - dev["wb-msw-v3_88"]["Temperature"];
+  dev["wbe2-i-ebus_12"]["Heating Setpoint"] = setpoint + Math.round(delta*k);
+}
+
+defineRule({
+  whenChanged: ["heat_floor/temperature"],
+  then: function (newValue, devName, cellName) {
+    setTemperature(newValue, dev["wb-w1/28-00000d6acdbe"]); 
+  }
+});
+
 defineRule("set_temperature", {
     when: cron("00 * * * *"),
     then: function(newValue, devName, cellName) {
-        outside_temp = dev["wb-w1/28-00000d6acdbe"];
-    	if((outside_temp > -40) && (outside_temp < -30)) {
-          setpoint = thermal_curve7[-40];
-        } else if ((outside_temp > -30) && (outside_temp < -20)) {
-          setpoint = thermal_curve7[-30];
-        } else if ((outside_temp > -20) && (outside_temp < -10.5)) {
-          setpoint = thermal_curve7[-20];
-        } else if ((outside_temp > -10.5) && (outside_temp < -6.5)) {
-          setpoint = thermal_curve7[-10.5];
-        } else if ((outside_temp > -6.5) && (outside_temp < -5)) {
-          setpoint = thermal_curve7[-6.5];
-        } else if ((outside_temp > -5) && (outside_temp < -3)) {
-          setpoint = thermal_curve7[-5];
-        } else if ((outside_temp > -3) && (outside_temp < 0)) {
-          setpoint = thermal_curve7[-3];
-        } else if ((outside_temp > 0) && (outside_temp < 1)) {
-          setpoint = thermal_curve7[0];
-        } else if ((outside_temp > 1) && (outside_temp < 2.5)) {
-          setpoint = thermal_curve7[1];
-        } else if ((outside_temp > 2.5) && (outside_temp < 5)) {
-          setpoint = thermal_curve7[2.5];
-        } else if ((outside_temp > 5) && (outside_temp < 7)) {
-          setpoint = thermal_curve7[5];
-        } else if ((outside_temp > 7) && (outside_temp < 8.5)) {
-          setpoint = thermal_curve7[7];
-        } else if ((outside_temp > 8.5) && (outside_temp < 10)) {
-          setpoint = thermal_curve7[8.5];
-        } else if (outside_temp > 10) {
-          setpoint = thermal_curve7[10];
-        }
-        
-        k = dev["heat_floor"]["k"]/10;
-        set_temp = dev["heat_floor"]["temperature"];
-      
-      	delta = set_temp - dev["wb-msw-v3_88"]["Temperature"];
-        //log.info("Температура снаружи", outside_temp);
-        //log.info("Уставка отопления", setpoint);
-        //log.info("Дельта", delta);
-        //log.info("Коэффициент", k);
-        //log.info("Уставка отопления с коррекцией", setpoint + Math.round(delta*k));
-      
-        dev["wbe2-i-ebus_12"]["Heating Setpoint"] = setpoint + Math.round(delta*k);
+      setTemperature(dev["heat_floor"]["temperature"], dev["wb-w1/28-00000d6acdbe"]);
     }
 });
